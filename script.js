b5caf172a4fdb8a01946c13d0f447fc708e2681e
Code <code@COCTL-41256.local>
2019-10-31 14:56:42 -0700

    added script

---
+++ script.js
@@ -0,0 +1,17 @@
+var randomButton = document.getElementById('generate'),
+    randomField = document.getElementById('random-student');
+
+const students = ['Jonathan Stewart', 'Joseph Perez','Nimish Lal','Jose angel Pantoja','Trevor Edwards','Jose Martinez','Johny Uribe','Deon Simmons','Robert Burns','Carter Galvez',
+'Zach Davis','Francisco Chavez','Kevin Cabello','Stephani Singh','Esteban Zaragoza','Carson Huber','Abraham Lopez','Bisola','David','Lourn Selga','Alex Nguyen','Nayo Ortz','Kurt Nipkau'];
+
+
+function getRandomStudents(){
+    return students[Math.floor(Math.random() * students.length)];
+}
+
+randomButton.addEventListener('click', function () {
+    console.log('');
+    randomField.innerText = getRandomStudents();
+    
+})
+
