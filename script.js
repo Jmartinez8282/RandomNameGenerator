b4dd4f5303de17f1c5795069df28e5c81ff25c66
Jose Martinez <josemartinez8282@gmail.com>
2019-12-21 05:53:21 -0800

    cp

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let randomButton = document.getElementById('generate'),
     randomField = document.getElementById('random-student');
 
-const students = ['Jonathan Stewart', 'Joseph Perez','Nimish Lal','Jose angel Pantoja','Trevor Edwards','Jose Martinez','Johny Uribe','Deon Simmons','Robert Burns','Carter Galvez',
+let students = ['Jonathan Stewart', 'Joseph Perez','Nimish Lal','Jose angel Pantoja','Trevor Edwards','Jose Martinez','Johny Uribe','Deon Simmons','Robert Burns','Carter Galvez',
 'Zach Davis','Francisco Chavez','Kevin Cabello','Stephani Singh','Esteban Zaragoza','Carson Huber','Abraham Lopez','Bisola Awujoola','David Abdallah','Lourn Selga','Alex Nguyen','Nayo Ortz','Kurt Nipkau'];
 
 
